9a7fb2d928b46f7150aa0960b8e95d0ed5b69b7e
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-05-07 08:21:23 -0700

    Completed buildPostOrderString and passes tests

--- src/Traversals.java
+++ src/Traversals.java
@@ -55,7 +55,12 @@ public class Traversals {
    * @return a post-order traversal string, or an empty string if the tree is null
    */
   public static <T> String buildPostOrderString(TreeNode<T> node) {
-    return null;
+    if(node == null) {
+      return "";
+    }
+    String leftString = buildPostOrderString(node.left);
+    String rightString = buildPostOrderString(node.right);
+    return leftString + rightString + node.value.toString();
   }
 
   /**
